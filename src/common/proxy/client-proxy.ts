7b293cde78653e7a42793bfd81c59c28ac4115fd
Darwin <salinash2000@gmail.com>
2021-12-01 15:29:28 -0600

    Passenger Controller

--- src/common/proxy/client-proxy.ts
+++ src/common/proxy/client-proxy.ts
@@ -19,4 +19,14 @@ export class ClientProxySuperFlights {
       },
     });
   }
+
+  clientProxyPassengers(): ClientProxy {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: this.config.get('AMQP_URL'),
+        queue: RabbitMQ.PassengerQueue,
+      },
+    });
+  }
 }
--- src/passenger/passenger.controller.ts
+++ src/passenger/passenger.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ClientProxySuperFlights } from '../common/proxy/client-proxy';
+import { PassengerDto } from './dto/passenger.dto';
+import { Observable } from 'rxjs';
+import { IPassenger } from 'src/common/interfaces/passenger.interface';
+import { PassengerMSG } from '../common/constants';
 
 @Controller('api/v2/passengers')
-export class PassengerController {}
+export class PassengerController {
+  constructor(private readonly clientProxy: ClientProxySuperFlights) {}
+
+  private clientProxyPassenger = this.clientProxy.clientProxyPassengers();
+
+  @Post()
+  create(@Body() passengerDto: PassengerDto): Observable<IPassenger> {
+    return this.clientProxyPassenger.send(PassengerMSG.CREATE, passengerDto);
+  }
+
+  @Get()
+  findAll(): Observable<IPassenger[]> {
+    return this.clientProxyPassenger.send(PassengerMSG.FIND_ALL, '');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<IPassenger> {
+    return this.clientProxyPassenger.send(PassengerMSG.FIND_ONE, id);
+  }
+
+  @Get(':id')
+  delete(@Param('id') id: string): Observable<IPassenger> {
+    return this.clientProxyPassenger.send(PassengerMSG.DELETE, id);
+  }
+
+  @Get(':id')
+  update(
+    @Param('id') id: string,
+    @Body() passengerDto: PassengerDto,
+  ): Observable<IPassenger> {
+    return this.clientProxyPassenger.send(PassengerMSG.UPDATE, {
+      id,
+      passengerDto,
+    });
+  }
+}
